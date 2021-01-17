7a22e0b989fab21b8fb7a74808e359f51c1cb255
Jaime Cabrera <cabrerajaimeandres@gmail.com>
2021-01-17 10:21:25 -0500

    Se agrego el estilo para la aplicacion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  const title = "To Do List"
+  return (
+    <div>
+      <h1 className="bg bg-color-dark">{title}</h1>
+    </div>
+  )
+}
+
+export default App;
